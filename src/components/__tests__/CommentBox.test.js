6b91207d46bee24061f50871b1edc0b7d3f2b52a
Stephen Zukowski <zukethenuke@hotmail.com>
2020-08-27 08:05:01 -0500

    Add CommentBox component and component tests

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = mount(<CommentBox /> );
+})
+
+afterEach(() => {
+    wrapped.unmount();
+})
+
+it('has a text area and a button', () => {
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('textarea').length).toEqual(1);
+})
+
+describe('the text area', () => {
+    beforeEach(() => {
+        wrapped
+        .find('textarea')
+        .simulate('change', { target: { value: 'new comment' } })
+        .update();
+    })
+
+    it('can be type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    })
+    
+    it('clears after a comment is submitted', () => {
+        wrapped
+            .simulate('submit')
+            .update();
+    
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    })
+})
