60bbd5e983041b7e1625d38c9afebb8ec4144e9f
Stephen Zukowski <zukethenuke@hotmail.com>
2020-09-01 09:37:41 -0500

    Add React Router and two routes

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -11,6 +11,7 @@ class CommentList extends Component {
         return (
             <div>
                 <ul>
+                    <h4>Comment List</h4>
                     {this.renderComments()}
                 </ul>
             </div>
