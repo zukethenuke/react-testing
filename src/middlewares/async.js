811eaed5ddb61164377867d1c83f2b9b024184a2
Stephen Zukowski <zukethenuke@hotmail.com>
2020-09-02 15:49:45 -0500

    Replace Redux-Promise with custom middleware

---
+++ src/middlewares/async.js
@@ -0,0 +1,19 @@
+// export default function({ dispatch }) {
+//     return function(next) {
+//         return function(action) {
+
+//         }
+//     }
+// }
+
+export default ({ dispatch }) => next => action => {
+    if(!action.payload || !action.payload.then) {
+        return next(action);
+    }
+
+    action.payload.then(response => {
+        const newAction = { ...action, payload: response };
+        dispatch(newAction);
+
+    })
+}
